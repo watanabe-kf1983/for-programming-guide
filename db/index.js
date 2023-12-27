5ace9a4f0335fffbe3746b012dea8ff74977bc98
Kiyofumi Watanabe <watanabe.kf1983@gmail.com>
2023-12-27 11:48:38 +0900

    ポスグレのssl接続対応

--- db/index.js
+++ db/index.js
@@ -1,11 +1,26 @@
 const pg = require("pg")
 
-const pool = new pg.Pool({
-    database: 'mydb',
-    user: 'watanabe',
-    password: 'pass',
-    max: 10
-})
+
+const isDbLocal = ((process.env.PGHOST || "localhost") === "localhost");
+
+function getConfig() {
+    if (isDbLocal) {
+        return {
+            max: 10
+        };
+
+    } else {
+        return {
+            max: 10,
+            ssl: {
+                rejectUnauthorized: false,
+            }
+        };
+    }
+}
+
+const pool = new pg.Pool(getConfig);
+
 
 async function query(text, params) {
     return await pool.query(text, params)
