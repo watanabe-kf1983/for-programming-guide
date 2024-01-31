86a95fb2830a159d00ddfd39475e6cf056be9dac
Kiyofumi Watanabe <watanabe.kf1983@gmail.com>
2024-01-31 16:54:01 +0900

    40回目の記事の3つ目の課題の解答例追加

--- controllers/router.js
+++ controllers/router.js
@@ -5,10 +5,18 @@ const namelist = require('../models/namelist');
 const family = require('../models/family');
 const auth = require('../auth/auth');
 
-router.get('/', function (req, res, next) {
-    const data = namelist();
-    data.history = req.session.history
-    data.username = req.user
+router.get('/', async function (req, res, next) {
+    const user = req.user;
+    const flatterees = await flatter.getFlatterees(user);
+    const flatterers = await flatter.getFlatterers(user);
+    const data = {
+        username: user,
+        namelist: namelist().namelist,
+        history: {
+            flatterees: flatterees,
+            flatterers: flatterers
+        },
+    };
     res.render('list_view.html', data);
 });
 
@@ -40,34 +48,20 @@ router.post('/remove-family', async function (req, res, next) {
 });
 
 
-router.get('/flatter/:name', function (req, res, next) {
-    const data = flatter(req.params.name);
-    pushHistory(req.session, req.params.name);
+router.get('/flatter/:name', async function (req, res, next) {
+    const data = await flatter.flatter(req.params.name, req.user);
     res.render('flatter_view.html', data);
 });
 
-router.get('/flatter-by-query', function (req, res, next) {
-    const data = flatter(req.query.name);
-    pushHistory(req.session, req.query.name);
+router.get('/flatter-by-query', async function (req, res, next) {
+    const data = await flatter.flatter(req.query.name, req.user);
     res.render('flatter_view.html', data);
 });
 
-router.post('/flatter-by-post', function (req, res, next) {
-    const data = flatter(req.body.name);
-    pushHistory(req.session, req.body.name);
+router.post('/flatter-by-post', async function (req, res, next) {
+    const data = await flatter.flatter(req.body.name, req.user);
     res.render('flatter_view.html', data);
 });
 
-router.get('/session-destroy', function (req, res, next) {
-    req.session.destroy();
-    res.redirect('/');
-});
-
-function pushHistory(session, name) {
-    if (!session.history) {
-        session.history = [];
-    }
-    session.history.push(name);
-}
 
 module.exports = router;
--- models/flatter.js
+++ models/flatter.js
@@ -1,20 +1,64 @@
-function flatter(name) {
+const db = require('../db');
+
+async function flatter(flatteree, flatterer) {
+
     const praise_words = ['素敵', 'スマート', 'オシャレ'];
     const flatter_words = [];
 
     for (word of praise_words) {
-        flatter_words.push(`${name}さんは${word}ですね`);
+        flatter_words.push(`${flatteree}さんは${word}ですね`);
     }
 
     praise_words.forEach(word => {
-        flatter_words.push(`${name}さんは本当に${word}ですね`);
+        flatter_words.push(`${flatteree}さんは本当に${word}ですね`);
     });
 
     for (let i = 0; i < praise_words.length; i++) {
-        flatter_words.push(`${name}さんはつくづく${praise_words[i]}ですね`);
+        flatter_words.push(`${flatteree}さんはつくづく${praise_words[i]}ですね`);
+    }
+    if (flatterer) {
+        await push_history(flatteree, flatterer);
     }
 
-    return { flatteree: { name: name }, words: flatter_words };
+    return { flatteree: { name: flatteree }, words: flatter_words };
 }
 
-module.exports = flatter;
+async function push_history(flatteree, flatterer) {
+    const client = await db.getClient();
+    try {
+        await client.query('BEGIN');
+        await client.query(
+            'INSERT INTO flatter_history VALUES ($1, $2, now())',
+            [flatterer, flatteree]
+        );
+        await client.query('COMMIT');
+
+    } catch (e) {
+        await client.query('ROLLBACK');
+        throw e;
+
+    } finally {
+        client.release();
+    }
+}
+
+async function getFlatterers(flatteree) {
+    result = await db.query(`SELECT DISTINCT flatterer 
+                            FROM flatter_history 
+                            WHERE flatteree = $1`, [flatteree]);
+    return result.rows.map(row => row.flatterer);
+}
+
+async function getFlatterees(flatterer) {
+    result = await db.query(`SELECT DISTINCT flatteree 
+                            FROM flatter_history 
+                            WHERE flatterer = $1`, [flatterer]);
+    return result.rows.map(row => row.flatteree);
+}
+
+
+module.exports = {
+    flatter: flatter,
+    getFlatterees: getFlatterees,
+    getFlatterers: getFlatterers
+};
